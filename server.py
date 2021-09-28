ea13190baaf661e976ebc708cb4f7d07f49d0c4d
Tate Merrill <tatemerrill@gmail.com>
2021-09-28 14:40:53 -0600

    finished

---
+++ server.py
@@ -0,0 +1,108 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+DISS = [
+  'weak', 'not as handsome as Bradley Pitt', 'not as cool as your mother thinks you are', 'not all jacked up on Mountain Dew'
+]
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+    <a href="http://localhost:5000/hello">Hi! This is the home page.</a>
+    </html>
+    """
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <input type="radio" id="comp_one" name="comp_choice" value="awesome">
+          <label for="comp_one">Awesome</label><br>
+          <input type="radio" id="comp_two" name="comp_choice" value="lovely">
+          <label for="comp_two">Lovely</label><br>
+          <input type="radio" id="comp_three" name="comp_choice" value="brilliant">
+          <label for="comp_three">Brilliant</label><br>
+          <input type="radio" id="comp_four" name="comp_choice" value="cool">
+          <label for="comp_four">Cool</label><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h2>You don't want this...</h2>
+        <form action="/diss">
+          Give me that name! <input type="text" name="dude">
+          <input type="submit" value="You're wild for this..">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    compliment = request.args.get("comp_choice")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+@app.route('/diss')
+def diss_person():
+
+  dude = request.args.get("dude")
+  diss = choice(DISS)
+
+  return f"""
+  <!doctype html>
+  <html>
+    <head>
+      <title>A Diss</title>
+    </head>
+    <body>
+      Hi, {dude}! I think you're {diss}!
+    </body>
+  </html>
+  """
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
